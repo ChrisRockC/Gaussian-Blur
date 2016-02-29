58cbc485d468d69aadb48dfc4ff36d39f2418e9c
陈超 <ccerdong@sina.cn>
2016-02-29 11:34:16 +0800

    iOS9 真机测试，设备：iphone6

--- Gaussian-blur/Gaussian-blur/ViewController.m
+++ Gaussian-blur/Gaussian-blur/ViewController.m
@@ -16,12 +16,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"高斯模糊";
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"2"]];
+    imageView.frame = CGRectMake(0,200,self.view.frame.size.width,200);
+    [self.view addSubview:imageView];
+    
+    
+    [self createBlurBackgroundWithImage:[UIImage imageNamed:@"2"] view:imageView blurRadius:50];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)createBlurBackgroundWithImage:(UIImage *)image view:(UIView *)view blurRadius:(CGFloat)blurRadius{
+    //1. 处理原始的NSData 数据
+    CIImage *originImage = [[CIImage alloc] initWithCGImage:image.CGImage];
+    
+    //2. 创建高斯模糊滤镜
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [filter setValue:originImage forKey:kCIInputImageKey];
+    [filter setValue:[NSNumber numberWithFloat:blurRadius] forKey:@"inputRadius"];
+    
+    //3. 生成模糊图片
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    UIImage *blurImage = [UIImage imageWithCGImage:[context createCGImage:result fromRect:result.extent]];
+    
+    //4. 将模糊图片加入背景
+    CGFloat w = self.view.frame.size.width;
+    CGFloat h = self.view.frame.size.height;
+    
+    UIImageView *blurImageView = [[UIImageView alloc] initWithFrame:CGRectMake(-w/2, -h/2, w*2, h*2)];
+    blurImageView.contentMode = UIViewContentModeScaleAspectFill; // 图片填充ImageView
+    // 保持原图长宽比
+    blurImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    blurImageView.image = blurImage;
+    
+    // 保证模糊背景在原图片view的下层
+    [self.view insertSubview:blurImageView belowSubview:view];
+    
+    
 }
-
 @end
